f4d15edf950ebd9c7f1f5f193c842876e5efbd5b
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 05:53:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def arrayCount(array)
   new_array = []
   counter = 0
   array.each do |person|
-    new_array << "#{counter + 1}. #{person}"
+    new_array << "#{counter}. #{person}"
     counter += 1
   end
-  return "The line is currently: #{array.join(" #{counter + 1}. ")}"
+  return "The line is currently: #{array.join(" #{counter}. ")}"
 end
 
 def line(array)
