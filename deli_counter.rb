d8e37c3cc17d46c0a4b51ed65c5c39898e6043fe
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 04:47:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(array)
   counter = 0
   array.each do |person|
     new_array << "#{counter + 1}. #{person}"
+    counter += 1
   end
   return "The line is currently: #{new_array.join(" ")}"
 end
