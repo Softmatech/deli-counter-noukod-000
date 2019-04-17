1ccb6d4c2afb3db3547faacfae8e834421cb8bfb
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 04:19:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,7 @@ def line(array)
   # "The line is currently: 1. Ada 2. Grace 3. Kent"
   if array.size > 0
     return "The line is currently: #{array[0, array.length].join('#{array.index} ')}"
+  else
+    return "The line is currently empty."
   end
 end
