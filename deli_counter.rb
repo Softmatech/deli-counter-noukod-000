29dfd77f9ad80ba0def05db126e23d0db1a2dbca
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 04:43:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@
 def line(array)
   # "The line is currently: 1. Ada 2. Grace 3. Kent"
   new_array = []
+  counter = 0
   array.each do |person|
-    
+    new_array << "#{counter + 1} #{person}"
   end
+  puts "#{new_array}"
 end
