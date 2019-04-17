628cb40a39af2c7c77ec552e2dad76a646562821
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 04:49:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,12 @@ def line(array)
   # "The line is currently: 1. Ada 2. Grace 3. Kent"
   new_array = []
   counter = 0
+  if array.size >0
   array.each do |person|
     new_array << "#{counter + 1}. #{person}"
     counter += 1
   end
   return "The line is currently: #{new_array.join(" ")}"
+else
+  return "The line is currently empty."
 end
