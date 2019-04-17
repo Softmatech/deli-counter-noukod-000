f3e685d0c69b0e9efa8f326ce62e4282ed4ad3be
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 05:24:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ def take_a_number(array,name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.size} in line."
 end
+
 def now_serving(array)
     array.each do |person|
       puts "Currently serving #{person}."
