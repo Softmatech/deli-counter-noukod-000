800a482730af5cab2f761807a4289a1a5ae82c09
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 05:21:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,8 @@ def take_a_number(array,name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.size} in line."
 end
+def now_serving(array)
+    if array.empty
+      puts "The line is currently empty."
+    end
+end
